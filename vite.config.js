f9bb34348812ec8853e7591b8cd8d19f75f94165
sephiaspp <sephiaseptiani4@upi.edu>
2026-08-21 11:51:42 +0700

    M7.2: verifikasi koneksi backend PHP - setup config.php, proxy Vite, PHP dev server, konfirmasi read.php status 200

--- vite.config.js
+++ vite.config.js
@@ -22,4 +22,12 @@ export default defineConfig({
       },
     }),
   ],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8000',
+        changeOrigin: true,
+      },
+    },
+  },
 })
