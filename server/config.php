e79324108b34fa89635b29e9099fb71c9685c74e
sephiaspp <sephiaseptiani4@upi.edu>
2026-08-27 18:21:52 +0700

    chore: stop tracking server/config.php, contains real DB credentials

--- server/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-return [
-    'dsn' => 'mysql:host=localhost;dbname=aftersch_bisnis_manajemen;charset=utf8mb4',
-    'username' => 'aftersch_bisnisuser',
-    'password' => 'Gn-@BS1U0DL3g!S&',
-];
