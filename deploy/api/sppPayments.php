e5b6c36c8bbb8757140058200a1296b882c61061
leyddddddd <barak.id551@gmail.com>
2026-08-31 14:36:48 +0700

    refactor: remove unused API endpoints and bootstrap functions

--- deploy/api/sppPayments.php
+++
@@ -1,6 +0,0 @@
-<?php
-declare(strict_types=1);
-require_once __DIR__ . '/../bootstrap.php';
-
-if ($_SERVER['REQUEST_METHOD'] !== 'POST') jsonResponse(['error' => 'Method tidak diizinkan'], 405);
-insertLedger('sppPayments', requestJson());
